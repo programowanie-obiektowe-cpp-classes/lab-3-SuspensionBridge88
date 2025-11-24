e63b73a4337e43768d9ef94a7bd08e6704d12163
SuspensionBridge88 <74118806+SuspensionBridge88@users.noreply.github.com>
2025-11-24 15:50:41 +0100

    Update transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+        return 0;
+
+    unsigned int suma = 0;
+    unsigned int zaglowce = 0;
+
+    Stocznia gdanska{};
+
+    while (suma < towar)
+    {
+        Statek* s1 = gdanska();
+        if (dynamic_cast< Zaglowiec* >(s1) != nullptr)
+            zaglowce++;
+        suma += s1->transportuj();
+
+        delete s1;
+    }
+
+    return zaglowce;
 }
